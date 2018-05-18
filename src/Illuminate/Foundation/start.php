b024c556a7bf0ba4f1277d584475931c2f92d5a5
Ivan Pagac <ivan@pagac.net>
2018-05-18 09:12:14 +0200

    ignore missing mcryt lib

--- src/Illuminate/Foundation/start.php
+++ src/Illuminate/Foundation/start.php
@@ -24,12 +24,12 @@ error_reporting(-1);
 |
 */
 
-if ( ! extension_loaded('mcrypt'))
+/*if ( ! extension_loaded('mcrypt'))
 {
 	echo 'Mcrypt PHP extension required.'.PHP_EOL;
 
 	exit(1);
-}
+}*/
 
 /*
 |--------------------------------------------------------------------------
